15a87a784cc7d25fc9638f2593f096247bde75cd
jesreel01 <jesreelmiole93@gmail.com>
2025-05-19 12:20:15 +0000

    Adds token verification and fixes user endpoint bugs
    Enhances security by implementing token verification for user creation endpoint.
    
    Fixes several issues in user endpoints:
    - Corrects method name from get_user_list to list_user
    - Fixes user_id parameter usage in get_user endpoint
    - Improves code formatting for better readability
    
    These changes strengthen API security and ensure proper functionality of user management operations.

--- server/app/api/v1/endpoints/user.py
+++ server/app/api/v1/endpoints/user.py
@@ -2,26 +2,33 @@ from typing import Annotated
 from fastapi import APIRouter, Depends
 from app.services.user_service import UserService
 from app.schema.user import UserCreate
+from app.core.security import verify_token
 
 router = APIRouter(prefix="/users", tags=["Users"])
 
 
 @router.post("")
-def create_user(user: UserCreate, user_service: Annotated[UserService, Depends(UserService)]):
-    print(user)
+def create_user(
+    user: UserCreate,
+    user_service: Annotated[UserService, Depends(UserService)],
+    token_data=Depends(verify_token),
+):
+    print(token_data)
     return user_service.create_user(user)
 
 
 @router.get("")
 def list_users(user_service: Annotated[UserService, Depends(UserService)]):
-    return user_service.get_user_list()
+    return user_service.list_user()
 
 
 @router.get("/{user_id}")
 def get_user(user_id: str, user_service: Annotated[UserService, Depends(UserService)]):
-    return user_service.get_user(id)
+    return user_service.get_user(user_id)
 
 
 @router.delete("/{user_id}")
-def delet_user(user_id: str,  user_service: Annotated[UserService, Depends(UserService)]):
+def delet_user(
+    user_id: str, user_service: Annotated[UserService, Depends(UserService)]
+):
     return user_service.delete_user(user_id)
